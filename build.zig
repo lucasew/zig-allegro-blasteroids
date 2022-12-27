dae9e338cac69c5f24cbbdbc2c5654be24ba679f
lucasew <lucas59356@gmail.com>
2022-12-26 21:58:03 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- build.zig
+++ build.zig
@@ -5,6 +5,9 @@ pub fn build(b: *Builder) !void {
     const exe = b.addExecutable("zblasteroids", "main.zig");
     exe.setOutputDir(".");
     exe.linkSystemLibrary("allegro");
+    exe.linkSystemLibrary("allegro_primitives");
+    exe.linkSystemLibrary("allegro_font");
+    exe.linkSystemLibrary("allegro_ttf");
     exe.linkSystemLibrary("c");
     b.default_step.dependOn(&exe.step);
 }
--- main.zig
+++ main.zig
@@ -2,8 +2,7 @@ const std = @import("std");
 const os = std.os;
 
 const print = std.debug.print;
-
-// const allocator = std.heap.page_allocator;
+const assert = std.debug.assert;
 
 const allegro = @cImport({
     @cInclude("allegro5/allegro.h");
@@ -15,6 +14,7 @@ const WindowTitle = "BLASTEROIDS by Lucas59356";
 
 var running = true;
 pub fn stop(_: c_int) callconv(.C) void {
+    print("stop triggered", .{});
     running = false;
 }
 
@@ -29,38 +29,45 @@ pub fn demo() !void {
 }
 
 pub fn setup_signals() void {
-    for (.{ os.SIG.TERM, os.SIG.INT }) |sig| {
+    inline for (.{ os.SIG.TERM, os.SIG.INT }) |sig| {
         os.sigaction(sig, &os.Sigaction{ .handler = .{ .handler = stop }, .mask = os.empty_sigset, .flags = 0 }, null);
     }
 }
 
+const GameInitError = error{ CantCreateQueue, CantCreateTimer, CantCreateDisplay, CantCreateFont, InvalidFontFile };
+
 const Game = struct {
-    timer: allegro.ALLEGRO_TIMER,
-    queue: allegro.ALLEGRO_EVENT_QUEUE,
-    display: allegro.ALLEGRO_DISPLAY,
-    font: allegro.ALLEGRO_FONT,
+    timer: *allegro.ALLEGRO_TIMER,
+    queue: *allegro.ALLEGRO_EVENT_QUEUE,
+    display: *allegro.ALLEGRO_DISPLAY,
+    font: *allegro.ALLEGRO_FONT,
 
-    pub fn new(allocator: *std.mem.Allocator, fps: i8) *Game {
+    pub fn new(allocator: *std.mem.Allocator, fps: f64) !*Game {
+        print("Creating game object", .{});
         var game = try allocator.create(Game);
-        errdefer allocator.destroy(game);
+        // errdefer allocator.free(game);
 
-        game.queue = allegro.al_create_event_queue();
+        game.queue = allegro.al_create_event_queue() orelse return GameInitError.CantCreateQueue;
 
-        game.timer = allegro.al_create_timer(1.0 / fps);
+        game.timer = allegro.al_create_timer(1.0 / fps) orelse return GameInitError.CantCreateTimer;
         allegro.al_start_timer(game.timer);
 
         allegro.al_register_event_source(game.queue, allegro.al_get_keyboard_event_source());
 
         allegro.al_set_new_display_flags(allegro.ALLEGRO_RESIZABLE);
-        game.display = allegro.al_create_display(600, 600);
+
+        game.display = allegro.al_create_display(600, 600) orelse return GameInitError.CantCreateDisplay;
         allegro.al_set_window_title(game.display, WindowTitle);
         allegro.al_register_event_source(game.queue, allegro.al_get_display_event_source(game.display));
-        game.font = allegro.al_load_font(std.process.getenv("ZBS_FONT"), 24, 0);
+
+        const font_file = std.os.getenv("ZAB_FONT") orelse return GameInitError.InvalidFontFile;
+        game.font = allegro.al_load_font(font_file, 24, 0) orelse return GameInitError.CantCreateFont;
 
         return game;
     }
 
-    pub fn destroy(self: Game, allocator: *std.mem.Allocator) void {
+    pub fn destroy(self: *Game, allocator: *std.mem.Allocator) void {
+        print("Destroying game object", .{});
         allegro.al_destroy_timer(self.timer);
         allegro.al_destroy_event_queue(self.queue);
         allegro.al_destroy_display(self.display);
@@ -72,6 +79,9 @@ const Game = struct {
 pub fn main() !void {
     try demo();
 
+    var memory: [64 * 1024 * 1024]u8 = undefined;
+    var allocator = std.heap.FixedBufferAllocator.init(&memory).allocator();
+
     setup_signals();
     defer stop(0);
 
@@ -80,6 +90,7 @@ pub fn main() !void {
     _ = allegro.al_init_font_addon();
     _ = allegro.al_init_ttf_addon();
 
-    var timer = allegro.al_create_timer(1.0 / 60.0);
-    allegro.al_start_timer(timer);
+    var game = try Game.new(&allocator, 30);
+    defer game.destroy();
+    std.time.sleep(1 * std.time.us_per_s);
 }
